918311cd21f2056b75cfaf901f06b4b618e7e37d
Felipe Pontes <felipemfpontes@gmail.com>
2018-12-19 13:45:44 -0300

    Add subscriptions

--- src/MessageList.tsx
+++ src/MessageList.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import ApolloClient from "apollo-client";
+import gql from "graphql-tag";
 
 type EventType = "join" | "left" | "message";
 
@@ -24,52 +25,87 @@ interface State {
 }
 
 export default class MessageList extends Component<Props, State> {
-  onMessage?: ZenObservable.Subscription;
-  onJoin?: ZenObservable.Subscription;
-  onLeft?: ZenObservable.Subscription;
+  onMessage: ZenObservable.Subscription;
+  onUserInteraction: ZenObservable.Subscription;
 
   container: React.RefObject<HTMLDivElement>;
 
   constructor(props: Props) {
     super(props);
 
+    const { client } = this.props;
+
     this.state = {
-      events: [
-        { type: "join", payload: { name: "Felipe" } },
-        { type: "join", payload: { name: "Chico" } },
-        { type: "join", payload: { name: "Marçal" } },
-        {
-          type: "message",
-          payload: { text: "Olá, todos", user: { name: "Felipe" } }
-        },
-        { type: "join", payload: { name: "Jota" } },
-        {
-          type: "message",
-          payload: { text: "Eaí, man", user: { name: "Jota" } }
-        },
-        { type: "join", payload: { name: "Felipe" } },
-        { type: "join", payload: { name: "Chico" } },
-        { type: "join", payload: { name: "Marçal" } },
-        {
-          type: "message",
-          payload: { text: "Olá, todos", user: { name: "Felipe" } }
-        },
-        { type: "join", payload: { name: "Jota" } },
-        {
-          type: "message",
-          payload: { text: "Eaí, man", user: { name: "Jota" } }
-        }
-      ]
+      events: []
     };
 
     this.container = React.createRef();
+
+    this.onMessage = client
+      .subscribe<OnMessageResult>({ query: ON_MESSAGE })
+      .subscribe(
+        ({ data }) => {
+          this.handleOnMessage(data.onMessage);
+        },
+        error => {
+          console.error(error);
+        }
+      );
+
+    this.onUserInteraction = client
+      .subscribe<OnUserInteractionResult>({ query: ON_USER_INTERACTION })
+      .subscribe(
+        ({ data }) => {
+          if (data.onJoin) {
+            this.handleOnJoin(data.onJoin);
+          }
+          if (data.onLeft) {
+            this.handleOnLeft(data.onLeft);
+          }
+        },
+        error => {
+          console.error(error);
+        }
+      );
   }
 
-  componentDidMount() {
+  handleOnMessage = (message: Message) => {
+    this.setState(
+      ({ events }) => ({
+        events: [...events, { type: "message", payload: message }]
+      }),
+      this.scrollToRecent
+    );
+  };
+
+  handleOnJoin = (user: User) => {
+    this.setState(
+      ({ events }) => ({
+        events: [...events, { type: "join", payload: user }]
+      }),
+      this.scrollToRecent
+    );
+  };
+
+  handleOnLeft = (user: User) => {
+    this.setState(
+      ({ events }) => ({
+        events: [...events, { type: "left", payload: user }]
+      }),
+      this.scrollToRecent
+    );
+  };
+
+  scrollToRecent = () => {
     const container = this.container.current;
     if (container) {
       container.scrollTop = container.scrollHeight - container.offsetHeight;
     }
+  };
+
+  componentWillUnmount() {
+    this.onMessage.unsubscribe();
+    this.onUserInteraction.unsubscribe();
   }
 
   render() {
@@ -111,3 +147,31 @@ export default class MessageList extends Component<Props, State> {
     );
   }
 }
+
+interface OnUserInteractionResult {
+  data: { onJoin?: User; onLeft?: User };
+}
+const ON_USER_INTERACTION = gql`
+  subscription {
+    onJoin {
+      name
+    }
+    onLeft {
+      name
+    }
+  }
+`;
+
+interface OnMessageResult {
+  data: { onMessage: Message };
+}
+const ON_MESSAGE = gql`
+  subscription {
+    onMessage {
+      text
+      user {
+        name
+      }
+    }
+  }
+`;
